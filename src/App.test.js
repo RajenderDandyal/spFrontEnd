99ee6ed3584dd996e8fa32c4afa26d2887148207
Raj <rajender.0184@gmail.com>
2020-05-02 17:11:44 +0530

    yup

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,20 @@
+import React from "react";
+import {shallow} from "enzyme";
+import App from "./App";
 
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-
-
-
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   const wrapper = shallow(<App />);
   const welcome = <p>Hi there!! how r u?</p>;
   // expect(wrapper.contains(welcome)).toBe(true);
-  expect(wrapper.contains(welcome)).toEqual(true)
+  expect(wrapper.contains(welcome)).toEqual(false);
 });
 
-it('renders without crashing', () => {
-  expect(2).toBe(2)
+it("renders without crashing", () => {
+  expect(2).toBe(2);
 });
-it('renders without crashing2', () => {
+it("renders without crashing2", () => {
   expect(2 + 2).toBe(4);
 });
-it('renders without crashing3', () => {
+it("renders without crashing3", () => {
   expect(2 + 2).toBe(4);
 });
